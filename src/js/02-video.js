6a578c216716552c1a55eaf24d93de2b99ca80eb
OksiWhiteleg <oksanabil99@gmail.com>
2023-07-30 18:35:01 +0200

    update

---
+++ src/js/02-video.js
@@ -0,0 +1,23 @@
+import Player from '@vimeo/player';
+import  throttle  from 'lodash.throttle';
+
+const player = new Player('vimeo-player');
+
+const onTime = function(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+player.on('timeupdate', throttle(onTime, 1000));
